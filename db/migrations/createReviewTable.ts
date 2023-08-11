f235e37526968b808bf78c287fa5673626e53461
Salman Ali <salmanpucit35@gmail.com>
2023-08-11 10:23:07 -0700

    add migration for reviews table

--- db/migrations/createReviewTable.ts
+++ db/migrations/createReviewTable.ts
@@ -1,10 +1,26 @@
 import db from "../db";
-import { faker } from "@faker-js/faker";
 
 // TODO: Empty migration
+const createReviewTableQuery = `
+  CREATE TABLE IF NOT EXISTS review(
+    id SERIAL PRIMARY KEY,
+    product_id INT NOT NULL,
+    rating INT CHECK (rating >= 1 AND rating <= 5),
+    timestamp TIMESTAMPTZ DEFAULT NOW(),
+    comment TEXT,
+    FOREIGN KEY (product_id) REFERENCES product (id)
+  )
+`;
 
 export const createReviewTable = async () => {
   console.log("Running db/migrations/createReviewTable migration...");
-
-  console.log("Completed db/migrations/createReviewTable migration.");
+  try {
+    console.log('Creating "reviews" table if it doesn\'t exist...');
+    await db.query(createReviewTableQuery);
+    console.log('Completed - Creating "review" table if it doesn\'t exist...');
+    console.log("✅ db/migrations/createReviewTable successful, review table created"
+    );
+  } catch (err) {
+    console.error("❌ Error with db/migrations/createReviewTable migration", err);
+  }
 };
